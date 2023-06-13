420f0cbfd6e9956651bc8a306d1cf42908150ece
忙巳 <109103173+lveMonsi@users.noreply.github.com>
2023-06-13 13:47:54 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,10 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur2173049-3a438b3b66586f3ed6fdff61',
   ],
 
   // 日志天数
@@ -22,15 +19,15 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'https://status.lvems.top//'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://github.com/lveMonsi'
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://lvems.top/'
     },
   ],
 };
